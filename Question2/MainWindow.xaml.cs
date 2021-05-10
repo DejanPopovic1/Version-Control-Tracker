138087ec4ed696f452b1abd2aee1d97c3ca6c495
Dejan Popovic <dejan.dp.popovic@gmail.com>
2021-05-10 03:13:47 +0200

    final commit

--- Question2/MainWindow.xaml.cs
+++ Question2/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -22,47 +23,142 @@ namespace Question2
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+    class Person
+    {
+        //There are 10 files in the DLL folder.
+        //We get the versions of all of the files.
+        //Should use file iterator here instead.
+        public void getVersions() {
+            var pathToDLLFile1 = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "myDLLs\\Antlr3.Runtime.dll");
+            var pathToDLLFile2 = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "myDLLs\\EntityFramework.dll");
+            var pathToDLLFile3 = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "myDLLs\\EntityFramework.SqlServer.dll");
+            var pathToDLLFile4 = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "myDLLs\\Microsoft.CodeDom.Providers.DotNetCompilerPlatform.dll");
+            var pathToDLLFile5 = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "myDLLs\\Microsoft.Web.Infrastructure.dll");
+            var pathToDLLFile6 = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "myDLLs\\MyApplication.dll");
+            var pathToDLLFile7 = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "myDLLs\\Newtonsoft.Json.dll");
+            var pathToDLLFile8 = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "myDLLs\\System.Web.Helpers.dll");
+            var pathToDLLFile9 = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "myDLLs\\System.Web.Mvc.dll");
+            var pathToDLLFile10 = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "myDLLs\\System.Web.Optimization.dll");
+
+            Assembly assembly1 = Assembly.LoadFrom(pathToDLLFile1);
+            Assembly assembly2 = Assembly.LoadFrom(pathToDLLFile2);
+            Assembly assembly3 = Assembly.LoadFrom(pathToDLLFile3);
+            Assembly assembly4 = Assembly.LoadFrom(pathToDLLFile4);
+            Assembly assembly5 = Assembly.LoadFrom(pathToDLLFile5);
+            Assembly assembly6 = Assembly.LoadFrom(pathToDLLFile6);
+            Assembly assembly7 = Assembly.LoadFrom(pathToDLLFile7);
+            Assembly assembly8 = Assembly.LoadFrom(pathToDLLFile8);
+            Assembly assembly9 = Assembly.LoadFrom(pathToDLLFile9);
+            Assembly assembly10 = Assembly.LoadFrom(pathToDLLFile10);
+
+            string[] result;
+        }
+    }
+
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            if (HelloButton.IsChecked == true)
-            {
-                MessageBox.Show("Hello.");
-            }
-            else if (GoodbyeButton.IsChecked == true)
-            {
-                MessageBox.Show("Goodbye.");
-            }
+            var myBinding = new Binding("ColorName");
+            
+            //TextBlock1.SetBinding(TextBlock.TextProperty, myBinding);
         }
 
         private void Button_Click2(object sender, RoutedEventArgs e)
         {
-            //string text = System.IO.File.ReadAllText(@".\TEST.txt");
-            //System.Console.WriteLine("Contents of WriteText.txt = {0}", text);
-            //Environment.NewLine("Hello, world!");
-            //Debug.WriteLine("Hellow World!");
-            //System.Diagnostics.Debug.WriteLine("Hello World!");
+            //There are 10 files in the DLL folder.
+            //We get the versions of all of the files.
+            //Should use file iterator here instead.
 
-            var path = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "myDLLs\\Test.txt");
-            //var path = Path.Combine(Directory.GetCurrentDirectory(), "\\TEST.txt");
-            string text = System.IO.File.ReadAllText(path);
-            MessageBox.Show(text);
+            var pathToDLLFile1 = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "myDLLs\\Antlr3.Runtime.dll");
+            var pathToDLLFile2 = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "myDLLs\\EntityFramework.dll");
+            var pathToDLLFile3 = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "myDLLs\\EntityFramework.SqlServer.dll");
+            var pathToDLLFile4 = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "myDLLs\\Microsoft.CodeDom.Providers.DotNetCompilerPlatform.dll");
+            var pathToDLLFile5 = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "myDLLs\\Microsoft.Web.Infrastructure.dll");
+            var pathToDLLFile6 = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "myDLLs\\MyApplication.dll");
+            var pathToDLLFile7 = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "myDLLs\\Newtonsoft.Json.dll");
+            var pathToDLLFile8 = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "myDLLs\\System.Web.Helpers.dll");
+            var pathToDLLFile9 = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "myDLLs\\System.Web.Mvc.dll");
+            var pathToDLLFile10 = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "myDLLs\\System.Web.Optimization.dll");
 
-            var pathToDLLFile = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "myDLLs\\Antlr3.Runtime.dll");
-            Assembly assembly = Assembly.LoadFrom(pathToDLLFile);
-            Version ver = assembly.GetName().Version;
-            MessageBox.Show(ver.ToString());
+            Assembly assembly1 = Assembly.LoadFrom(pathToDLLFile1);
+            Assembly assembly2 = Assembly.LoadFrom(pathToDLLFile2);
+            Assembly assembly3 = Assembly.LoadFrom(pathToDLLFile3);
+            Assembly assembly4 = Assembly.LoadFrom(pathToDLLFile4);
+            Assembly assembly5 = Assembly.LoadFrom(pathToDLLFile5);
+            Assembly assembly6 = Assembly.LoadFrom(pathToDLLFile6);
+            Assembly assembly7 = Assembly.LoadFrom(pathToDLLFile7);
+            Assembly assembly8 = Assembly.LoadFrom(pathToDLLFile8);
+            Assembly assembly9 = Assembly.LoadFrom(pathToDLLFile9);
+            Assembly assembly10 = Assembly.LoadFrom(pathToDLLFile10);
+
+            String ver1 = assembly1.GetName().Version.ToString();
+            String ver2 = assembly2.GetName().Version.ToString();
+            String ver3 = assembly3.GetName().Version.ToString();
+            String ver4 = assembly4.GetName().Version.ToString();
+            String ver5 = assembly5.GetName().Version.ToString();
+            String ver6 = assembly6.GetName().Version.ToString();
+            String ver7 = assembly7.GetName().Version.ToString();
+            String ver8 = assembly8.GetName().Version.ToString();
+            String ver9 = assembly9.GetName().Version.ToString();
+            String ver10 = assembly10.GetName().Version.ToString();
+
+           
+
+            //MessageBox.Show(ver.ToString());
         }
 
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
+
+
+
+
+
+
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private MainWindow Model;
+        public MainWindowViewModel(MainWindow model)
+        {
+            this.Model = model;
+        }
+
+        public string Name
+        {
+            get { return Model.Name; }
+            set
+            {
+                Model.Name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            var e = new PropertyChangedEventArgs(propertyName);
+            PropertyChangedEventHandler changed = PropertyChanged;
+            if (changed != null) changed(this, e);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
-
-
-//Assembly assembly = Assembly.LoadFrom("TestAssembly.dll");
-//Version ver = assembly.GetName().Version;
